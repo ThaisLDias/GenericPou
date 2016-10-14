e3a37d3c700e326b28850f14c77d1a087c427ee5
Mauricio Leonardo <victorfernandes.nave@gmail.com>
2016-10-14 14:57:56 -0300

    Alpha Version

--- app/src/main/java/com/example/thaisdias/jamvpoupou/MainActivity.java
+++ app/src/main/java/com/example/thaisdias/jamvpoupou/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.thaisdias.jamvpoupou;
 
+import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -8,74 +9,111 @@ import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     SharedPreferences sh;
+    boolean pouIsAlive = true;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent t = new Intent(this,PouService.class);
+        if(!isMyServiceRunning(PouService.class))
+        startService(t);
+
         sh = this.getSharedPreferences("poujamv", Context.MODE_PRIVATE);
-        setImage();
+
+        if(sh.getFloat("Hunger",50f) <= 0 || sh.getFloat("Thirsty",50f) <= 0)
+        {
+            ((Button)findViewById(R.id.food)).setVisibility(View.GONE);
+            ((Button)findViewById(R.id.water)).setVisibility(View.GONE);
+            ((Button)findViewById(R.id.revive)).setVisibility(View.VISIBLE);
+            Bitmap bm = BitmapFactory.decodeResource(this.getResources(),R.drawable.dead);
+            ImageView view = (ImageView)findViewById(R.id.imageView);
+            view.setImageBitmap(bm);
+        }
+        else
+        {
+            setImage();
+            ((TextView)findViewById(R.id.ft)).setText(sh.getFloat("Hunger",50f)+"");
+            ((TextView)findViewById(R.id.wt)).setText(sh.getFloat("Thirsty",50f)+"");
+        }
     }
 
-    void setImage(){
-
-        if(sh.getFloat("Hunger",50f) > 25f) { /*põe pra mudar imagem-(celoba)*/
+    void setImage()
+    {
+        if(sh.getFloat("Hunger",50f) > 25f)
+        {
+            /*põe pra mudar imagem-(celoba)*/
             Bitmap bm = BitmapFactory.decodeResource(this.getResources(),R.drawable.celoba);
             ImageView view = (ImageView)findViewById(R.id.imageView);
             view.setImageBitmap(bm);
         }
-        if(sh.getFloat("Hunger",50f) <= 24f) {
+        if(sh.getFloat("Hunger",50f) <= PouService.LIMIT)
+        {
             Bitmap b = BitmapFactory.decodeResource(this.getResources(),R.drawable.sad);
             ImageView view = (ImageView)findViewById(R.id.imageView);
             view.setImageBitmap(b);
         }
     }
 
+    public void revivePou(View v)
+    {
+        SharedPreferences.Editor ed = sh.edit();
+        ed.putFloat("Hunger",50);
+        ed.putFloat("Thirsty",50);
+        ed.apply();
+        Intent i = new Intent(this,MainActivity.class);
+        finish();
+        startActivity(i);
+    }
+
     @Override
-    protected void onResume() {
+    protected void onResume()
+    {
         super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Intent t = new Intent(this,PouService.class);
-        startService(t);
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Intent t = new Intent(this,PouService.class);
-        startService(t);
     }
 
     public void giveFood(View v)
     {
-        Toast.makeText(this,"Celoba is already full of love!",Toast.LENGTH_LONG).show();
+        //Toast.makeText(this,"Celoba is already full of love!",Toast.LENGTH_SHORT).show();
         SharedPreferences.Editor ed = sh.edit();
         ed.putFloat("Hunger",sh.getFloat("Hunger",50) +25);
         if(sh.getFloat("Hunger",50) > 50) {ed.putFloat("Hunger",50);}
         if(sh.getFloat("Hunger",50) > 25) PouService.hungry = false;
         ed.apply();
         setImage();
+        ((TextView)findViewById(R.id.ft)).setText(sh.getFloat("Hunger",50f)+"");
     }
 
     public void giveWater(View v)
     {
-        Toast.makeText(this,"Celoba is hydrated!",Toast.LENGTH_LONG).show();
+        //Toast.makeText(this,"Celoba is hydrated!",Toast.LENGTH_SHORT).show();
         SharedPreferences.Editor ed = sh.edit();
         ed.putFloat("Thirsty",sh.getFloat("Thirsty",50) +25);
         if(sh.getFloat("Thirsty",50) > 50) {ed.putFloat("Thirsty",50);}
         if(sh.getFloat("Thirsty",50) > 25) PouService.thirsty = false;
         ed.apply();
         setImage();
+        ((TextView)findViewById(R.id.wt)).setText(sh.getFloat("Thirsty",50f)+"");
+    }
+
+    private boolean isMyServiceRunning(Class<?> serviceClass)
+    {
+        ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE))
+        {
+            if (serviceClass.getName().equals(service.service.getClassName())) return true;
+        }
+        return false;
     }
 
 }
--- app/src/main/java/com/example/thaisdias/jamvpoupou/PouService.java
+++ app/src/main/java/com/example/thaisdias/jamvpoupou/PouService.java
@@ -10,12 +10,14 @@ import android.content.SharedPreferences;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class PouService extends Service {
 
     public static Boolean hungry = false;
     public static Boolean thirsty = false;
+    public static float LIMIT = 10f;
     public PouService() {
     }
 
@@ -27,55 +29,45 @@ public class PouService extends Service {
 
     private void Send()
     {
-
         SharedPreferences sh;
         sh = this.getSharedPreferences("poujamv", Context.MODE_PRIVATE);
-        if(sh.getFloat("Hunger",50)<25) {hungry = true; Log.d("Disgurpe",hungry+"");}
-        else {thirsty = true;Log.d("Disgurpe2",thirsty+"");}
-        //Building the notification.
+        if(sh.getFloat("Hunger",50)<LIMIT) {hungry = true;}
+        else {thirsty = true;Log.d("Disgurpe2",thirsty+"");}//n sei
         NotificationCompat.Builder mBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(this)
                 .setSmallIcon(R.drawable.celoba)
                 .setContentTitle((sh.getFloat("Hunger",50)<25)?"Celoba is hungry!":"Celoba is thirsty!")
                 .setContentText((sh.getFloat("Hunger",50)<25)?"Feed him!":"Give him water!")
                 .setAutoCancel(true);
-
-        //Intent to be started if user activate the notification panel.
         PendingIntent pi = PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
-
-        //Connecting notification with intent.
         mBuilder.setContentIntent(pi);
-
-        //Need permission on manifest.
         mBuilder.setVibrate(new long[] {100, 250, 100, 500});
-
-        //Getting the OS notification service.
         NotificationManager mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-
-        //Sending the notification.
         mNotificationManager.notify(R.string.app_name, mBuilder.build());
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            // Restore interrupt status.
-            Thread.currentThread().interrupt();
-        }
+        final SharedPreferences sh = this.getSharedPreferences("poujamv", Context.MODE_PRIVATE);
+        final Context c = this;
+        new Thread(new Runnable(){
+            public void run() {
+                // TODO Auto-generated method stub
+                while(true)
+                {
+                    try {
+                        Thread.sleep(3000);
+                    }catch (InterruptedException e){}
+                    SharedPreferences.Editor ed = sh.edit();
+                    ed.putFloat("Thirsty",sh.getFloat("Thirsty",50) -2);
+                    if(sh.getFloat("Thirsty",50)<0)ed.putFloat("Thirsty",0);
+                    ed.putFloat("Hunger",sh.getFloat("Hunger",50) -1);
+                    if(sh.getFloat("Hunger",50)<0)ed.putFloat("Hunger",0);
+                    ed.apply();
+                    if ((sh.getFloat("Thirsty",50) <= LIMIT && !thirsty) || (sh.getFloat("Hunger",50) <= LIMIT && !hungry)) {Send();}
+                }
+            }
+        }).start();
 
-        SharedPreferences sh;
-        sh = this.getSharedPreferences("poujamv", Context.MODE_PRIVATE);
-
-        SharedPreferences.Editor ed = sh.edit();
-        ed.putFloat("Thirsty",sh.getFloat("Thirsty",50) -2);
-        if(sh.getFloat("Thirsty",50)<0)ed.putFloat("Thirsty",0);
-        ed.putFloat("Hunger",sh.getFloat("Hunger",50) -1);
-        if(sh.getFloat("Hunger",50)<0)ed.putFloat("Hunger",0);
-        ed.apply();
-        if ((sh.getFloat("Thirsty",50) <= 24 && !thirsty) || (sh.getFloat("Hunger",50) <= 24 && !hungry)) {Send();}
-
-        startService(intent);
         // If we get killed, after returning from here, restart
         return START_STICKY;
     }
